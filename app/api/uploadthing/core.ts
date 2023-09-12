f4869b2e5425d2634d0181756a1543dc8104fdbd
speedytman <wdcoker3@gmail.com>
2023-09-12 14:53:17 -0400

    Added file upload function

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,22 @@
+import { auth } from "@/lib/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+ 
+const f = createUploadthing();
+ 
+const handleAuth = async () => {
+  const session = await auth();
+  if(!session) throw new Error("Unauthorized")
+  return { userId: session.user.id }
+}
+ 
+
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1}})
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {})
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
