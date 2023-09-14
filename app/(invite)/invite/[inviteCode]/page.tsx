c54b57541bfb1884fa5689f2ea4b8de22ac79bbe
speedytman <wdcoker3@gmail.com>
2023-09-14 14:19:02 -0400

    Added invite code functionality

---
+++ app/(invite)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,59 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface InviteCodeProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCode: React.FC<InviteCodeProps> = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/login");
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
+};
+
+export default InviteCode;
