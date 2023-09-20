e394c16be13113d6372861115e8880e694c6e13a
speedytman <wdcoker3@gmail.com>
2023-09-19 20:49:56 -0400

    added file upload to messages

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -21,16 +21,60 @@ const FileUpload: React.FC<FileUploadProps> = ({
   const fileType = value?.split(".").pop();
   if (value && fileType !== "pdf") {
     return (
-      <div className="relative h-20 w-20">
-        <Image
-          fill
-          src={value}
-          alt="Upload"
-          className="rounded-full border border-zinc-400"
-        />
+      <>
+        {endpoint === "serverImage" && (
+          <div className="relative h-20 w-20">
+            <Image
+              fill
+              src={value}
+              alt="Upload"
+              className="rounded-full border border-zinc-400"
+            />
+            <button
+              onClick={() => onChange("")}
+              className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+              type="button"
+            >
+              <X className="h-4 w-4" />
+            </button>
+          </div>
+        )}
+        {endpoint === "messageFile" && (
+          <div className="relative h-64 w-64">
+            <Image
+              fill
+              src={value}
+              alt="Upload"
+              className="rounded-lg border border-zinc-400"
+            />
+            <button
+              onClick={() => onChange("")}
+              className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+              type="button"
+            >
+              <X className="h-4 w-4" />
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
         <button
           onClick={() => onChange("")}
-          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
